34db6c7c965d8969597c609a1548948cc26b078c
rahmannurhidayat022 <rahmannurhidayat022@gmail.com>
2022-08-21 15:07:21 +0700

    styled dashboard

--- resources/views/auth/dashboard.blade.php
+++ resources/views/auth/dashboard.blade.php
@@ -2,23 +2,23 @@
 
 @section('sidecontent')
 
-<div class="pt-4">
+<div class="pt-4 container">
     <div class="card border-0 p-5 shadow-sm">
         <div class="card-body">
             <div class="card-title h4 mb-4">Dashboard</div>
             <div class="row">
                 <div class="col-3">
                     <a style="text-decoration: none;" class="text-dark" href="#">
-                        <div class="card p-4 d-flex align-items-center justify-content-center shadow-sm">
-                            <img width="80px" src="{{ asset('icons/chevron-up.svg') }}" alt="dollar">
+                        <div style="min-height: 200px" class="card border border-success p-4 d-flex align-items-center justify-content-center">
+                            <img width="60px" src="{{ asset('icons/chevron-up.svg') }}" alt="dollar">
                             <p class="text-center mt-4">Perhitungan Jangka Pendek</p>
                         </div>
                     </a>
                 </div>
                 <div class="col-3">
                     <a style="text-decoration: none;" class="text-dark" href="#">
-                        <div class="card p-4 d-flex align-items-center justify-content-center shadow-sm">
-                            <img width="80px" src="{{ asset('icons/chevrons-up.svg') }}" alt="dollar">
+                        <div style="min-height: 200px" class="card border border-success p-4 d-flex align-items-center justify-content-center">
+                            <img width="60px" src="{{ asset('icons/chevrons-up.svg') }}" alt="dollar">
                             <p class="text-center mt-4">Perhitungan Jangka Panjang</p>
                         </div>
                     </a>
--- resources/views/auth/layout.blade.php
+++ resources/views/auth/layout.blade.php
@@ -1,26 +1,24 @@
 @extends('app')
 
 @section('content')
-
-<div class="container overflow-hidden">
-    <div class="row gx-5">
-        <div class="col-2 bg-success bg-gradient pt-4" style="height: 100vh">
+    <div class="row w-100">
+        <div class="col-2 bg-success bg-gradient pt-5 ps-4" style="height: 100vh">
             <h1 class="h3 text-white">Earned Value Management (EVM)</h1>
             <hr>
             <ul class="nav flex-column" style="font-weight: bold">
-                <li class="nav-item d-flex align-items-center">
-                    <img width="18px" src="{{ asset('icons/user.svg') }}" alt="user icon">
+                <li class="nav-item d-flex align-items-center py-2 ps-3 rounded bg-light my-1">
+                    <img width="20px" src="{{ asset('icons/user.svg') }}" alt="user icon">
                     <a class="nav-link text-dark active" aria-current="page" href="#">
                         User
                     </a>
                 </li>
-                <li class="nav-item d-flex align-items-center">
+                <li class="nav-item d-flex align-items-center py-2 ps-3 rounded bg-light my-1">
                     <img class="me-1" width="18px" src="{{ asset('icons/database.svg') }}" alt="user icon">
                     <a class="nav-link text-dark" href="#">
                         Data Proyek
                     </a>
                 </li>
-                <li class="nav-item d-flex align-items-center">
+                <li class="nav-item d-flex align-items-center py-2 ps-3 rounded bg-light my-1">
                     <img width="18px" src="{{ asset('icons/divide.svg') }}" alt="user icon">
                     <a class="nav-link text-dark" href="#">
                         Hasil Perhitungan
@@ -32,5 +30,4 @@
             @yield('sidecontent')
         </div>
     </div>
-</div>
 @endsection
